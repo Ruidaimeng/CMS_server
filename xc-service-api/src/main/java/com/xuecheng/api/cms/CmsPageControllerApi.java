277ea3140f4950714a507e1aff3cf5b542356cf0
Ruimeng <13816570705@163.com>
2018-11-28 17:14:21 +0800

    1

--- xc-service-api/src/main/java/com/xuecheng/api/cms/CmsPageControllerApi.java
+++ xc-service-api/src/main/java/com/xuecheng/api/cms/CmsPageControllerApi.java
@@ -2,7 +2,21 @@ package com.xuecheng.api.cms;/*
  * Description
  *@author Ruimeng
  *@Date 2018/11/27 17:20
+ * 页面相关接口
  */
 
+import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
+import com.xuecheng.framework.model.response.QueryResponseResult;
+
 public interface CmsPageControllerApi {
+
+    //页面查询，分页查询
+
+    /**
+     * @param page
+     * @param size
+     * @param queryPageRequest 页面查询条件
+     * @return
+     */
+    public QueryResponseResult findList(int page , int size, QueryPageRequest queryPageRequest);
 }
